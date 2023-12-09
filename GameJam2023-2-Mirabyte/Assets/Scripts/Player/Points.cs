25f9bd59c37b547b8f099d6bf4f5eed06efe942a
bakosrozsa <bakosrozsa01@gmail.com>
2023-12-10 00:41:54 +0100

    Added point counter

--- GameJam2023-2-Mirabyte/Assets/Scripts/Player/Points.cs
+++ GameJam2023-2-Mirabyte/Assets/Scripts/Player/Points.cs
@@ -1,20 +1,25 @@
 using Player;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Points : MonoBehaviour
 {
     public byte point;
+    public TextMeshProUGUI counter;
     void Start()
     {
         point = 0;
+        counter.text = point.ToString();
     }
     void Update()
     {
-        if (gameObject.GetComponent<PlayerBehaviour>().thiefStunned % 5 == 0 && point != 0) 
+        /*if (gameObject.GetComponent<PlayerBehaviour>().thiefStunned % 5 == 0 && point != 0) 
         {
             point += 1;
-        }
+            counter.text = point.ToString();
+        }*/
     }
 }
