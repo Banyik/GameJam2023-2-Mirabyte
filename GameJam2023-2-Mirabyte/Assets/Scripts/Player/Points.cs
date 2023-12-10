e6c31d4f4afadcd17008cf4177ee8d4e3e3e4117
bakosrozsa <bakosrozsa01@gmail.com>
2023-12-10 10:52:06 +0100

    Added point and thief stun counter

--- GameJam2023-2-Mirabyte/Assets/Scripts/Player/Points.cs
+++ GameJam2023-2-Mirabyte/Assets/Scripts/Player/Points.cs
@@ -8,18 +8,28 @@ using UnityEngine.UI;
 public class Points : MonoBehaviour
 {
     public byte point;
-    public TextMeshProUGUI counter;
+    bool otodikUtes;
+    public GameObject player;
     void Start()
     {
         point = 0;
-        counter.text = point.ToString();
+        otodikUtes = false;
     }
     void Update()
     {
-        /*if (gameObject.GetComponent<PlayerBehaviour>().thiefStunned % 5 == 0 && point != 0) 
+        if (player.GetComponent<PlayerBehaviour>().thiefStunned == 5 && point == 0 && !otodikUtes)
         {
             point += 1;
-            counter.text = point.ToString();
-        }*/
+            otodikUtes = true;
+        }
+        else if (player.GetComponent<PlayerBehaviour>().thiefStunned % 5 == 0 && point != 0 && !otodikUtes) 
+        {
+            point += 1;
+            otodikUtes = true;
+        }
+        else if(otodikUtes && player.GetComponent<PlayerBehaviour>().thiefStunned % 5 != 0)
+        {
+            otodikUtes = false;
+        }
     }
 }
