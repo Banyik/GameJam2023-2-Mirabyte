070c1a4c259348fc92f74cde79e63319fffe0697
bakosrozsa <bakosrozsa01@gmail.com>
2023-12-10 01:55:24 +0100

    Added cursor changing

--- GameJam2023-2-Mirabyte/Assets/Scripts/Cursor/CursorHandler.cs
+++ GameJam2023-2-Mirabyte/Assets/Scripts/Cursor/CursorHandler.cs
@@ -1,15 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CursorHandler : MonoBehaviour
 {
     public Texture2D taser;
+    public Texture2D cross;
     public CursorMode cursorMode = CursorMode.ForceSoftware;
     public Vector2 hotSpot = Vector2.zero;
+    string currentScene;
     void Start()
     {
-        Cursor.SetCursor(taser, hotSpot, cursorMode);
+        currentScene = SceneManager.GetActiveScene().name;
+        switch (currentScene)
+        {
+            case "Menu":
+                Cursor.SetCursor(taser, hotSpot, cursorMode);
+                break;
+            case "MovementTest":
+                Cursor.SetCursor(cross, hotSpot, cursorMode);
+                break;
+            default:
+                break;
+        }
+    }
+    private void Update()
+    {
+        if (currentScene == "MovementTest")
+        {
+            InGameCursorChange();
+        }
+    }
+    void InGameCursorChange() 
+    {
+        if (gameObject.GetComponent<PauseMenu>().GameIsPaused)
+        {
+            Cursor.SetCursor(taser, hotSpot, cursorMode);
+        }
+        else
+        {
+            Cursor.SetCursor(cross, hotSpot, cursorMode);
+        }
     }
 
     private void OnMouseEnter()
